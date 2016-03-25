4113d80a9c7c209bc6f787ce769cbf1158e5d036
gio <gio@gio-deb>
2016-03-25 23:55:20 +0100

    Add looping 3 times

--- break_time.py
+++ break_time.py
@@ -1,5 +1,11 @@
 import webbrowser
 import time
 
-time.sleep(10)  # time in seconds to change in the final version
-webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+total_breaks = 3
+break_count = 0
+
+print("This program started on " + time.ctime()) # print current time
+while(break_count < total_breaks):
+    time.sleep(10)  # time in seconds to change in the final version
+    webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    break_count = break_count + 1
