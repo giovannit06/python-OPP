88f825c3f4463b2299c399f5c6cc1ccd15f47a44
gio <gio@gio-deb>
2016-03-26 19:54:07 +0100

    Add Get file names part

--- rename_files.py
+++ rename_files.py
@@ -1,5 +1,8 @@
+import os
 def rename_files():
     # (1) get file names from a folder
+    file_list = os.listdir("/home/gio/python/python-OPP/prank/")
+    print(file_list)
 
     # (2) for each file, rename filename
 
