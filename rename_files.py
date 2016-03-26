8230580b47cf7c3901fb464ecd5d0753c780e4f9
gio <gio@gio-deb>
2016-03-26 21:05:39 +0100

    Add save and restore path and loop to rename file

--- rename_files.py
+++ rename_files.py
@@ -2,8 +2,12 @@ import os
 def rename_files():
     # (1) get file names from a folder
     file_list = os.listdir("/home/gio/python/python-OPP/prank/")
-    print(file_list)
-
+    # print(file_list)
+    saved_path = os.getcwd()
+    print("Current Working Directory is " + saved_path)
+    os.chdir("/home/gio/python/python-OPP")
     # (2) for each file, rename filename
-
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, "0123456789"))
+    os.chdir(saved_path)
 rename_files()
