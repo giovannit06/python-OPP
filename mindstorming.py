91e641ee340df58d6642aa08a4540d1542a30432
gio <gio@gio-deb>
2016-03-27 21:03:12 +0200

    Refactoring code for draw a square

--- mindstorming.py
+++ mindstorming.py
@@ -1,23 +1,20 @@
 import turtle
 
-def draw_square():
+def draw_square(some_turtle):
+    for i in range(1, 5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+def draw_art():
     window = turtle.Screen()
     window.bgcolor("red")
-    
+    # Create the turtle Brad - Draw a square
     brad = turtle.Turtle()
-    brad.shape("classic")
+    brad.shape("circle")
     brad.color("green")
     brad.speed(1)
-
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-
+    draw_square(brad)
+    # Create the turtle Angie - Draw a circle
     angie = turtle.Turtle()
     angie.shape("arrow")
     angie.color("blue")
@@ -25,5 +22,5 @@ def draw_square():
 
     window.exitonclick()
 
-draw_square()
+draw_art()
     
